a33ce843bb1b5c5c1cabf8ab92c1b48aeb16fb49
Mariana <mmarimary26@gmail.com>
2024-04-05 17:06:58 -0300

    feat: Implementing event routes and event handler

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,5 +1,8 @@
 from flask import flask
 from flask_cors import CORS
+from src.models.settings.connection import db_connection_handler
+
+db_connection_handler.connect_to_db()
 
 app = Flast(__name__)
 CORS(app)
